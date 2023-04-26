68707c40cadac9119a44f7a455755ae2607a4d6c
Verred <obregon_portal@outlook.com>
2023-04-25 21:05:24 -0500

    Fixed problem to main folder

--- BusInclusivo/data.js
+++ BusInclusivo/data.js
@@ -1,23 +1,6 @@
 module.exports = function () {
   var data = {
-    marca: [
-      {
-        id:1,
-        descripcion: "Ford",
-      },
-      {
-        id:2,
-        descripcion: "Lexus",
-      },
-      {
-        id:3,
-        descripcion: "Susuki",
-      },
-      {
-        id:4,
-        descripcion: "Hyunday",
-      }
-    ]
+  
   }
   return data
 }
