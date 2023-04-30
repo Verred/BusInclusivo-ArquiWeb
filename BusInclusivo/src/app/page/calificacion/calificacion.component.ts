0d93527011e140fc08c1bfac4897ebe949169f26
Main <lokireyes482@gmail.com>
2023-04-30 10:08:40 -0500

    arreglo de error en el listar

--- BusInclusivo/src/app/page/calificacion/calificacion.component.ts
+++ BusInclusivo/src/app/page/calificacion/calificacion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CalificacionListarComponent } from './calificacion-listar/calificacion-listar.component';
 
 @Component({
   selector: 'app-calificacion',
